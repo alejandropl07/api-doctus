3a36e185e228e65d510cabe57b17ba8dde858d11
alejandropl07 <aprietoleon97@gmail.com>
2022-03-03 23:23:28 -0600

    Agregado morgan y nodemon

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const morgan  = require("morgan");
 
-//app.use('/api/libros');
-
+//app.use(morgan('start'));
 
 
 var corsOptions = {
